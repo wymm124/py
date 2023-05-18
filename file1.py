83e0cb2afc8ab93768f4dd23d6d6c60698e73431
wymm124 <w1y2m4m@yeah.net>
2023-05-19 00:22:31 +0800

    open file

---
+++ file1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2023/5/19 0:18
+# @Author  : 日积跬步
+# @File    : file1.py
+
+file = open('a.txt', 'r')
+print(file.readlines())
+file.close()
+
